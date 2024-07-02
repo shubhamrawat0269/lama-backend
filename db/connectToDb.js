6438626ab01068490d857b1eef9630c01d021ada
shubham Rawat <rawatshubam269@gmail.com>
2024-07-02 08:47:25 +0530

    bug fix

--- db/connectToDb.js
+++ db/connectToDb.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose");
 
-const connectToDb = () => {
-  mongoose
-    .connect(`${process.env.DB_URI}`)
-    .then(() => console.log(`Connected to DB at ${process.env.DB_URI}`))
-    .catch((e) => console.log("Failed to connect to DB", e));
+const connectToDb = async () => {
+  // mongoose
+  //   .connect(`${process.env.DB_URI}`)
+  //   .then(() => console.log(`Connected to DB at ${process.env.DB_URI}`))
+  //   .catch((e) => console.log("Failed to connect to DB", e));
+
+  try {
+    await mongoose.connect(`${process.env.DB_URI}`);
+    console.log(`Connected to DB at ${process.env.DB_URI}`);
+  } catch (error) {
+    console.log(`Failed to connect to MongoDB`, error);
+  }
 };
 
 module.exports = connectToDb;
